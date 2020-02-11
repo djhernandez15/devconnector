2fd880ae0c08533afe7e49a3b805d30ba2201ba7
David Hernandez Arias <david.hernandez3@coxautoinc.com>
2020-02-11 09:22:24 -0600

    added gravatar

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,59 @@
 const express = require('express');
+const bcrypt = require('bcryptjs0.');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @route    POST api/users
 // @desc     Register user
 // @access   Public
-router.post('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required')
+      .not()
+      .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password must be at least 6 characters').isLength({
+      min: 6
+    })
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Return jsonwebtoken
+      res.send('User route');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+    //
+  }
+);
 
 module.exports = router;
